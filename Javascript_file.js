38805b75c9e0a40bb9f80e50210103320e1369b3
Greg Hartjen <31355614+greghartjen@users.noreply.github.com>
2020-01-10 14:00:00 -0800

    update Javascript file

--- Javascript_file.js
+++ Javascript_file.js
@@ -1,1 +1,6 @@
-alert("Hello, World!");
+document.write("Hello, World!<br>");
+var X = "Learning how to use variables, sweet!<br>";
+document.write(X);
+var windowAlert = "This is my windows alert!";
+alert(windowAlert);
+document.write("\"Learning Javascript is fun\" he exclaimed.<br>");
